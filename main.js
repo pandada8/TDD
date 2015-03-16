e3c2dc2d869892773c43915b113f0b077fdad8ad
pandada8 <pandada8@gmail.com>
2015-03-17 00:47:17 +0800

    update to v2

--- main.js
+++ main.js
@@ -1,51 +1,78 @@
 var _ = require('./node_modules/lodash');
-function Fight(master_a, master_b){
+
+exports.Fight = function Fight(master_a, master_b){
     this.a = master_a;
     this.b = master_b;
-    
-}
-exports.Fight = Fight;
-Fight.prototype.record = function(attacker, victim){
-};
-Fight.prototype.round = function(){
-    // First a should attack b
-    
-}
-Fight.prototype.finished = function(){
-    return !this.a.life || !this.b.life;
-}
-Fight.prototype.showWinner = function(){
-    var winner = this.a.life > this.b.life ? this.a : this.b;
-    var result = _.template('<%= winner.name %> win the game!')({winner:winner});
-    console.log(result)
-}
-Fight.prototype.start = function(){
-    while(!this.finished()){
-        var round = this.round();
-        log(round)
-    }
-    this.showWinner();
 }
 
-function Monster(name, life, aggressivity){
+exports.Fight.prototype.finished = function(){
+    return this.a.life <= 0 || this.b.life <= 0;
+}
+
+exports.Fight.prototype.Winner = function(){
+    var winner = this.a.life > this.b.life ? this.a : this.b;
+    return winner;
+}
+
+exports.Fight.prototype.logWinner = function(){
+    var winner = this.Winner();
+    exports.log(_.template("<%= name %>胜利")(winner))
+}
+
+exports.Fight.prototype.start = function(){
+    var flag = true;
+    // the flag is a good idea :/
+    while(!this.finished()){
+        if (flag){
+            this.a.attack(this.b)
+        }else{
+            this.b.attack(this.a)
+        }
+        flag = !flag
+    }
+    this.logWinner();
+}
+
+exports.Monster = function Monster(name, life, aggressivity){
     this.name = name;
     this.life = life;
     this.aggressivity = aggressivity;
-    this.role = "Normal Guy"
+    this.role = "普通人";
 }
-function Hero(name, life, aggressivity){
-    Monster.call(this, name, life, aggresivity);
-    this.role = "Hero"
+exports.Hero = function Hero(name, life, aggressivity){
+    exports.Monster.call(this, name, life, aggressivity);
+    this.role = "战士";
+    this.weapon = arguments.length == 5 ? arguments[3] : null;
+    this.protection = arguments.length == 5 ? arguments[4] : null;
 }
-function log(words){
+exports.Hero.constructor = exports.Hero;
+
+exports.Monster.prototype.attack = function attack_by_monster(victim){
+    // should we just write this in one line or split it into multi if ?
+    var attack = !_.isEmpty(victim.protection) ? (this.aggressivity < victim.protection.value ? 0 : this.aggressivity - victim.protection.value) : this.aggressivity
+    victim.life -= attack;
+    var str = _.template('<%= a.role %><%= a.name %><% print(a.weapon ? "用" + a.weapon.name : "") %>攻击了<%= b.role %><%= b.name %>,' +
+    '<%= b.name %>受到了<%= attack %>点伤害, <%= b.name %>的剩余生命值<%= b.life %>')({
+        a:this,
+        b:victim,
+        attack:attack
+    })
+    exports.log(str)
+}
+
+
+exports.Hero.prototype.attack = function attack_by_hero(victim){
+    var attack = this.aggressivity + (!_.isEmpty(this.weapon) ? this.weapon.value : 0) - (!_.isEmpty(victim.protection) ? victim.protection.value : 0);
+    victim.life -= attack;
+    var str = _.template('<%= a.role %><%= a.name %><% print(a.weapon ? "用" + a.weapon.name : "") %>攻击了<%= b.role %><%= b.name %>,' +
+    '<%= b.name %>受到了<%= attack %>点伤害, <%= b.name %>的剩余生命值<%= b.life %>')({
+        a:this,
+        b:victim,
+        attack:attack
+    })
+    exports.log(str)
+}
+
+exports.log = function log(words){
     console.log(words);
-    return words;
-}
-exports.Monster = Monster;
-exports.Hero = Hero;
-Monster.prototype.attck = function(monster){
-    var attck_number = this.aggressivity > monster.life ? monster.life : this.aggressivity;
-    monster.life -= attck_number;
-    // show the log
-    console.log(this.name+"攻击"+monster.name+"，"+monster.name+"受到" +attck_number+ "的伤害，"+monster.name+"剩余生命值"+monster.life)
 }
--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -1,19 +1,89 @@
-// Add this to your vimrc to add keymap to run the test
-// 
-//
-
 // include the lib
 
 var game = require('../main.js')
-describe("Let's fight, just a test suit for fighting game",function(){
-    it("should print a result",function(){
-        spyOn(console, 'log');
+
+describe("战斗吧少年！",function(){
+    beforeEach(function(){
+        game.logs = [];
+        game.log = function(words){
+            game.logs.push(words);
+        }
+    })
+
+    it("返回赢家并打印",function(){
         var A = {life: 10, name:"A"};
         var B = {life: 0, name:"B"};
         var fight = new game.Fight(A, B);
-        fight.showWinner();
+        var winner = fight.Winner();
 
-        expect(console.log).toHaveBeenCalledWith('A win the game!')
+        expect(winner.name).toBe(A.name)
+        fight.logWinner()
+        expect(game.logs.slice(-1)[0]).toBe("A胜利")
+    })
 
+    it("当两人属性一致时，先手胜", function(){
+        var A = {life: 10, name: "A"};
+        A.attack = function(b){
+            b.life -= 10;
+        }
+        var B = {life: 10, name: "B"};
+        B.attack = function(a){
+            a.life -= 10;
+        }
+        var fight = new game.Fight(A, B);
+        fight.start();
+        var winner = fight.Winner();
+
+        expect(winner.name).toBe(A.name)
+    })
+
+    it('不同属性的两个人，按照攻击力来判断', function(){
+        var A = {life: 10, name: 'A', aggressivity: 5};
+        var B = {life: 15, name: "B", aggressivity: 10};
+        B.attack = function(o){
+            // 简单的减攻击力
+            o.life -= this.aggressivity
+        }
+        A.attack = function(o){
+            o.life -= this.aggressivity
+        }
+        var fight = new game.Fight(A, B);
+        fight.start()
+        var winner = fight.Winner();
+
+        expect(winner.name).toBe(B.name)
+
+    })
+
+    it('攻击时输出Log', function(){
+        var A = {life: 10, name: "A"};
+        var B = {life: 10, name: "B"};
+        function Attack (obj) {
+            obj.life -= 10;
+            game.log(this.name +" Attack");
+        }
+        B.attack = Attack.bind(B)
+        A.attack = Attack.bind(A)
+        var fight = new game.Fight(A, B);
+        fight.start()
+        var winner = fight.Winner();
+
+        expect(game.logs.join('|')).toBe("A Attack|A胜利");
+    })
+
+    it("测试Log_两个正常人", function(){
+        var A = new game.Monster('张三', 10, 20);
+        var B = new game.Monster('李四', 10, 20);
+        var fight = new game.Fight(A, B);
+        fight.start()
+        expect(game.logs.join('|')).toBe("普通人张三攻击了普通人李四,李四受到了20点伤害, 李四的剩余生命值-10|张三胜利")
+    })
+
+    it("测试Log_使用装备", function(){
+        var A = new game.Hero('张三', 40, 20, {name:'木剑', value:20}, {});
+        var B = new game.Monster('李四', 100, 20, {}, {name:"XXX", value:10});
+        var fight = new game.Fight(A, B);
+        fight.start()
+        expect(game.logs.join('|')).toBe("战士张三用木剑攻击了普通人李四,李四受到了40点伤害, 李四的剩余生命值60|普通人李四攻击了战士张三,张三受到了20点伤害, 张三的剩余生命值20|战士张三用木剑攻击了普通人李四,李四受到了40点伤害, 李四的剩余生命值20|普通人李四攻击了战士张三,张三受到了20点伤害, 张三的剩余生命值0|李四胜利")
     })
 })
