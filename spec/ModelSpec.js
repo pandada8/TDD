52b69b2a293ae93f8acc2c236e1f3e8064a7728f
pandada8 <pandada8@gmail.com>
2015-03-21 15:22:35 +0800

    move the test of model to anothor file

---
+++ spec/ModelSpec.js
@@ -0,0 +1,40 @@
+var game = require('../main.js')
+describe("测试 Log 与 攻击", function(){
+     it("测试Log_两个正常人", function(){
+        var A = new game.model.Player('张三', 10, 20);
+        var B = new game.model.Player('李四', 10, 20);
+        var text = A.attack(B);
+        expect(text).toBe("普通人张三攻击了普通人李四,李四受到了20点伤害, 李四的剩余生命值-10")
+    })
+
+    it("测试Log_有武器战士攻击正常人", function(){
+        var A = new game.model.Hero('张三', 40, 20, {name:'木剑', value:20}, {});
+        var B = new game.model.Player('李四', 100, 20);
+        var text = A.attack(B);
+        expect(text).toBe("战士张三用木剑攻击了普通人李四,李四受到了40点伤害, 李四的剩余生命值60");
+    })
+    it("测试Log_有盾战士攻击正常人", function(){
+        var A = new game.model.Hero('张三', 40, 20, {}, {name: "盾", value: 10});
+        var B = new game.model.Player('李四', 100, 20 );
+        var text = A.attack(B)
+        expect(text).toBe("战士张三攻击了普通人李四,李四受到了20点伤害, 李四的剩余生命值80");
+    })
+    it("测试Log_普通人攻击有盾战士", function(){
+        var A = new game.model.Player('张三', 100, 20);
+        var B = new game.model.Hero('李四', 40, 20, {},{name: "盾", value: 10}) ;
+        var text = A.attack(B)
+        expect(text).toBe("普通人张三攻击了战士李四,李四受到了10点伤害, 李四的剩余生命值30")
+    })
+    it("测试Log_普通人攻击有剑战士", function(){
+        var A = new game.model.Player('张三', 100, 20);
+        var B = new game.model.Hero('李四', 40, 20, {name:"木剑", value: 10}) ;
+        var text = A.attack(B)
+        expect(text).toBe("普通人张三攻击了战士李四,李四受到了20点伤害, 李四的剩余生命值20");
+    })
+    it("测试Log_普通人攻击有剑有盾的战士", function(){
+        var A = new game.model.Player('张三', 100, 20);
+        var B = new game.model.Hero('李四', 40, 20, {name:"木剑", value: 10}) ;
+        var text = A.attack(B);
+        expect(text).toBe("普通人张三攻击了战士李四,李四受到了20点伤害, 李四的剩余生命值20")
+    })
+})
--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -76,48 +76,6 @@ describe("战斗吧少年！",function(){
         expect(logger.logs.join('|')).toBe("A Attack|A胜利");
     })
 
-    it("测试Log_两个正常人", function(){
-        console.log(game)
-        var A = new game.model.Player('张三', 10, 20);
-        var B = new game.model.Player('李四', 10, 20);
-        var fight = new game.Fight(A, B, logger);
-        fight.start()
-        expect(logger.logs.join('|')).toBe("普通人张三攻击了普通人李四,李四受到了20点伤害, 李四的剩余生命值-10|张三胜利")
-    })
 
-    it("测试Log_有武器战士攻击正常人", function(){
-        var A = new game.model.Hero('张三', 40, 20, {name:'木剑', value:20}, {});
-        var B = new game.model.Player('李四', 100, 20);
-        var fight = new game.Fight(A, B, logger);
-        fight.start()
-        expect(logger.logs.join('|')).toBe("战士张三用木剑攻击了普通人李四,李四受到了40点伤害, 李四的剩余生命值60|普通人李四攻击了战士张三,张三受到了20点伤害, 张三的剩余生命值20|战士张三用木剑攻击了普通人李四,李四受到了40点伤害, 李四的剩余生命值20|普通人李四攻击了战士张三,张三受到了20点伤害, 张三的剩余生命值0|李四胜利")
-    })
-    it("测试Log_有盾战士攻击正常人", function(){
-        var A = new game.model.Hero('张三', 40, 20, {}, {name: "盾", value: 10});
-        var B = new game.model.Player('李四', 100, 20 );
-        var fight = new game.Fight(A, B, logger);
-        fight.start()
-        expect(logger.logs.join('|')).toBe("战士张三攻击了普通人李四,李四受到了20点伤害, 李四的剩余生命值80|普通人李四攻击了战士张三,张三受到了10点伤害, 张三的剩余生命值30|战士张三攻击了普通人李四,李四受到了20点伤害, 李四的剩余生命值60|普通人李四攻击了战士张三,张三受到了10点伤害, 张三的剩余生命值20|战士张三攻击了普通人李四,李四受到了20点伤害, 李四的剩余生命值40|普通人李四攻击了战士张三,张三受到了10点伤害, 张三的剩余生命值10|战士张三攻击了普通人李四,李四受到了20点伤害, 李四的剩余生命值20|普通人李四攻击了战士张三,张三受到了10点伤害, 张三的剩余生命值0|李四胜利");
-    })
-    it("测试Log_普通人攻击有盾战士", function(){
-        var A = new game.model.Player('张三', 100, 20);
-        var B = new game.model.Hero('李四', 40, 20, {},{name: "盾", value: 10}) ;
-        var fight = new game.Fight(A, B, logger);
-        fight.start()
-        expect(logger.logs.join('|')).toBe("普通人张三攻击了战士李四,李四受到了10点伤害, 李四的剩余生命值30|战士李四攻击了普通人张三,张三受到了20点伤害, 张三的剩余生命值80|普通人张三攻击了战士李四,李四受到了10点伤害, 李四的剩余生命值20|战士李四攻击了普通人张三,张三受到了20点伤害, 张三的剩余生命值60|普通人张三攻击了战士李四,李四受到了10点伤害, 李四的剩余生命值10|战士李四攻击了普通人张三,张三受到了20点伤害, 张三的剩余生命值40|普通人张三攻击了战士李四,李四受到了10点伤害, 李四的剩余生命值0|张三胜利")
-    })
-    it("测试Log_普通人攻击有剑战士", function(){
-        var A = new game.model.Player('张三', 100, 20);
-        var B = new game.model.Hero('李四', 40, 20, {name:"木剑", value: 10}) ;
-        var fight = new game.Fight(A, B, logger);
-        fight.start()
-        expect(logger.logs.join('|')).toBe("普通人张三攻击了战士李四,李四受到了20点伤害, 李四的剩余生命值20|战士李四用木剑攻击了普通人张三,张三受到了30点伤害, 张三的剩余生命值70|普通人张三攻击了战士李四,李四受到了20点伤害, 李四的剩余生命值0|张三胜利");
-    })
-    it("测试Log_普通人攻击有剑有盾的战士", function(){
-        var A = new game.model.Player('张三', 100, 20);
-        var B = new game.model.Hero('李四', 40, 20, {name:"木剑", value: 10}) ;
-        var fight = new game.Fight(A, B, logger);
-        fight.start()
-        expect(logger.logs.join('|')).toBe("普通人张三攻击了战士李四,李四受到了20点伤害, 李四的剩余生命值20|战士李四用木剑攻击了普通人张三,张三受到了30点伤害, 张三的剩余生命值70|普通人张三攻击了战士李四,李四受到了20点伤害, 李四的剩余生命值0|张三胜利")
-    })
+
 })
