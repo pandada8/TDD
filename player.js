abad935642fd3a15db1496c9e6ae2e54bd291559
pandada8 <pandada8@gmail.com>
2015-03-21 16:00:16 +0800

    remove the weapon and protection of the normal player

--- player.js
+++ player.js
@@ -4,8 +4,6 @@ function Player(name, life, aggressivity){
     this.life = life;
     this.aggressivity = aggressivity;
     this.role = '普通人';
-    this.weapon = {};
-    this.protection = {};
 }
 
 Player.prototype.calcDamage = function CalcuteDamage(victim){
@@ -20,8 +18,12 @@ Player.prototype.attack = function attack_by_monster(victim){
     // should we justwrite this in one line or split it into multi if ?
     var attack = this.calcDamage(victim);
     victim.life -= attack;
-    var str = _.template('<%= a.role %><%= a.name %><% print(a.weapon.value ? "用" + a.weapon.name : "") %>攻击了<%= b.role %><%= b.name %>,' +
-    '<%= b.name %>受到了<%= attack %>点伤害, <%= b.name %>的剩余生命值<%= b.life %>')({
+    var str = _.template('<%= a.role %><%= a.name %><% print(_.isEmpty(a.weapon) ? "" : "用" + a.weapon.name) %>攻击了<%= b.role %><%= b.name %>,' +
+    '<%= b.name %>受到了<%= attack %>点伤害, <%= b.name %>的剩余生命值<%= b.life %>', {
+        imports:{
+            '_': _
+        }
+    })({
         a:this,
         b:victim,
         attack:attack
